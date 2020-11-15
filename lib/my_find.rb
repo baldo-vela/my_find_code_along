c572f741cb57d7ee473b671ba263cf4ac82dbcba
baldo-vela <baldo.vela+github@gmail.com>
2020-11-15 04:33:05 -0600

    commit tree cc4011dea16c045e7f06db0c85fa100848dce34c

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_find(collection)
-
-end
+    i = 0
+    while i < collection.length
+      return collection[i] if yield(collection[i])
+      #refactored to verify the block
+      i = i + 1
+    end
+  end 
